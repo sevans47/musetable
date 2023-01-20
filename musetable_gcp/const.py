cb282f1dd36e1fa2672795025eaf83a5c649af8d
stephen evans <stephenpevans47@gmail.com>
2023-01-20 21:46:17 +0900

    added gcp functionality

---
+++ musetable_gcp/const.py
@@ -0,0 +1,8 @@
+import os
+
+my_path = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(my_path, os.pardir))
+
+# google cloud storage
+PROJECT_ID = 'audio-projects-363306'
+BUCKET_NAME = 'musetable'
