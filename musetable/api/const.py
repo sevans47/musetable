c091c4c1d9a01fccc2687c5b7623ee68b170e73f
stephen evans <stephenpevans47@gmail.com>
2024-12-31 15:27:49 +0900

    begin work on fastapi

--- musetable/api/const.py
+++ musetable/api/const.py
@@ -1,7 +1,7 @@
 import os
 
 my_path = os.path.dirname(os.path.abspath(__file__))  # get path to directory with const.py
-ROOT_DIR = os.path.abspath(os.path.join(my_path, os.pardir))  # get path to parent dir of const.py
+ROOT_DIR = os.path.abspath(os.path.join(my_path, os.pardir, os.pardir))  # get path to parent dir of const.py
 
 SCOPE = 'local'
 # SCOPE = 'flask'  # changing scope will adjust how file upload works
